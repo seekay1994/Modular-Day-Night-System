6342396c093ae091fe88c5e04c0282cdc5ae357a
seekay <calvin.kliese@live.de>
2024-08-30 18:54:25 +0200

    Update updateColorInstantly.js

--- updateColorInstantly.js
+++ updateColorInstantly.js
@@ -12,33 +12,18 @@ export var scriptProperties = createScriptProperties()
         label: 'Night Color',
         value: new Vec3(0, 0, 0) // Default black color
     })
-    .addText({
-        name: 'sharedValueName',
-        label: 'Shared Value Name',
-        value: 'sharedColor'
-    })
     .finish();
 
-// Function to determine if it is day or night based on the current time
-function isDayTime() {
-    let currentTime = engine.timeOfDay;
-    return currentTime >= 0.275 && currentTime < 0.875;
-}
 
 export function update(value) {
-    let currentState = shared.currentState;
+    let currentState = shared.currentTODState;
     let newColor;
 
     if (currentState === 'day') {
         newColor = scriptProperties.dayColor;
     } else if (currentState === 'night') {
         newColor = scriptProperties.nightColor;
-    } else if (currentState === 'automatic') {
-        newColor = isDayTime() ? scriptProperties.dayColor : scriptProperties.nightColor;
     }
-
-    // Save the color to the shared value
-    shared[scriptProperties.sharedValueName] = newColor;
 
     return newColor;
 }
