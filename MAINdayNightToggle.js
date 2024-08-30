e10c88e67e6d0d4907900368813a4c15ce91357f
seekay <calvin.kliese@live.de>
2024-08-31 00:31:50 +0200

    Update MAINdayNightToggle.js
    Fixed broken time check.

--- MAINdayNightToggle.js
+++ MAINdayNightToggle.js
@@ -1,7 +1,6 @@
 'use strict';
 
 export var scriptProperties = createScriptProperties()
-
     .addSlider({
         name: 'dayStart',
         label: 'Day Start',
@@ -23,6 +22,7 @@ export var scriptProperties = createScriptProperties()
 // Define the sprite sheet states
 const states = ['automatic', 'day', 'night'];
 let currentStateIndex = 0;
+let lastKnownState = '';
 
 export function init() {
     // Load the state from local storage
@@ -34,27 +34,46 @@ export function init() {
     // Pause the texture animation
     let anim = thisLayer.getTextureAnimation();
     anim.pause();
-    
+
     // Set the initial frame
     anim.setFrame(currentStateIndex);
-    
+
     // Set the initial shared state
     shared.currentTODState = getSharedState();
+
+    // Set last known state
+    lastKnownState = shared.currentTODState;
 }
 
 export function cursorClick(event) {
     // Update the state index to the next state
     currentStateIndex = (currentStateIndex + 1) % states.length;
-    
+
     // Get the texture animation and set the frame
     let anim = thisLayer.getTextureAnimation();
     anim.setFrame(currentStateIndex);
-    
+
     // Update the shared state value
     shared.currentTODState = getSharedState();
 
     // Save the current state to local storage
     localStorage.set('currentTimeState', states[currentStateIndex]);
+
+    // Update last known state
+    lastKnownState = shared.currentTODState;
+}
+
+export function update() {
+    if (states[currentStateIndex] === 'automatic') {
+        let currentState = getSharedState();
+        if (currentState !== lastKnownState) {
+            // State has changed, update the animation frame and shared state
+            lastKnownState = currentState;
+            let anim = thisLayer.getTextureAnimation();
+            anim.setFrame(states.indexOf(currentState));
+            shared.currentTODState = currentState;
+        }
+    }
 }
 
 function isDayTime() {
