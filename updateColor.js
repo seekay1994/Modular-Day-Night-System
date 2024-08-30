b9943ec534ab9e7a7b591ad192a638c6f951c159
seekay <calvin.kliese@live.de>
2024-08-30 18:54:07 +0200

    Update updateColor.js

--- updateColor.js
+++ updateColor.js
@@ -22,18 +22,7 @@ export var scriptProperties = createScriptProperties()
         max: 10,
         integer: false
     })
-    .addText({
-        name: 'sharedValueName',
-        label: 'Shared Value Name',
-        value: 'sharedColor'
-    })
     .finish();
-
-// Function to determine if it is day or night based on the current time
-function isDayTime() {
-    let currentTime = engine.timeOfDay;
-    return currentTime >= 0.275 && currentTime < 0.875;
-}
 
 let targetColor;
 let smoothColor;
@@ -46,22 +35,17 @@ export function init(value) {
 }
 
 export function update(value) {
-    let currentState = shared.currentState;
+    let currentState = shared.currentTODState;
     let newColor;
 
     if (currentState === 'day') {
         newColor = scriptProperties.dayColor;
     } else if (currentState === 'night') {
         newColor = scriptProperties.nightColor;
-    } else if (currentState === 'automatic') {
-        newColor = isDayTime() ? scriptProperties.dayColor : scriptProperties.nightColor;
     }
 
     // Smooth transition
     smoothColor = smoothColor.mix(newColor, Math.min(1.0, engine.frametime * transitionSpeed));
-
-    // Save the color to the shared value
-    shared[scriptProperties.sharedValueName] = smoothColor;
 
     return smoothColor;
 }
