c40302171b7b49fd70e09d51a3f1fc99a318be09
seekay <calvin.kliese@live.de>
2024-08-30 18:56:18 +0200

    Update updateValueInstantly.js

--- updateValueInstantly.js
+++ updateValueInstantly.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Customizable sliders and text for day and night values and shared value name
 export var scriptProperties = createScriptProperties()
     .addSlider({
         name: 'dayValue',
@@ -18,49 +17,29 @@ export var scriptProperties = createScriptProperties()
         max: 1,
         integer: false
     })
-    .addText({
-        name: 'sharedValueName',
-        label: 'Shared Value Name',
-        value: 'sharedDnNValue'
-    })
     .finish();
 
 let currentValue;
 
-function isDayTime() {
-    let currentTime = engine.timeOfDay;
-    return currentTime >= 0.275 && currentTime < 0.875;
-}
-
 export function init(value) {
     // Initialize currentValue based on initial state
-    let initialState = shared.currentState || 'automatic';
+    let initialState = shared.currentTODState;
     if (initialState === 'day') {
         currentValue = scriptProperties.dayValue;
     } else if (initialState === 'night') {
         currentValue = scriptProperties.nightValue;
-    } else {
-        currentValue = isDayTime() ? scriptProperties.dayValue : scriptProperties.nightValue;
     }
-
-    // Save the initial value to the shared value
-    shared[scriptProperties.sharedValueName] = currentValue;
 }
 
 export function update(value) {
-    let currentState = shared.currentState;
+    let currentState = shared.currentTODState;
     let newValue;
 
     if (currentState === 'day') {
         newValue = scriptProperties.dayValue;
     } else if (currentState === 'night') {
         newValue = scriptProperties.nightValue;
-    } else if (currentState === 'automatic') {
-        newValue = isDayTime() ? scriptProperties.dayValue : scriptProperties.nightValue;
     }
-
-    // Save the value to the shared value
-    shared[scriptProperties.sharedValueName] = newValue;
 
     return newValue;
 }
